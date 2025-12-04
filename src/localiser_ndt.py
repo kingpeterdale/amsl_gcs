0be85f3b5590fb09900d85f2a49f9becba0db6d5
PDKING <kingpeterdale@gmail.com>
2025-12-04 13:45:51 +1100

    updated map generator. implemented very basic grid localisation

--- src/localiser_ndt.py
+++ src/localiser_ndt.py
@@ -3,19 +3,20 @@ import pandas as pd
 import cv2
 
 class LaserLocator():
-    def __init__(self):
+    def __init__(self, init=(0.,0.,0.)):
         
         # Static Base Map
-        self.static_map = cv2.imread('survival_pool.png',cv2.IMREAD_GRAYSCALE)
+        self.static_map = cv2.imread('/home/peter/code/AMSL/amsl_gcs/src/survival_pool.png',cv2.IMREAD_GRAYSCALE)
 
         # Laser Scans
         self.scan_angles = np.linspace(np.radians(0),np.radians(360),720)
         self.laser_file = open('/home/peter/code/AMSL/amsl_gcs/src/logs/20251119_145433/gcs_145433.log')
 
         # State Estimate
-        self.dx = 0
-        self.dy = 0
-        self.dh = 0
+        self.dx = init[0]
+        self.dy = init[1]
+        self.dh = init[2]
+
 
     def get_next_scan(self):
         for line in self.laser_file:
@@ -25,20 +26,85 @@ class LaserLocator():
                 return scan
     
 
+    def start_grid_based(self,local_search_window=(5,5,5)):
+        '''
+        For each scan, searches a local grid to find best match.
+        local grid is relative to current estimate.
+        Initial guess must be provided
+        
+        :param local_search_window: size of local area to search
+        '''
+        grid_x = np.arange(self.dx-local_search_window[0],self.dx+local_search_window[0]+1)
+        grid_y = np.arange(self.dy-local_search_window[1],self.dy+local_search_window[1]+1)
+        grid_h = np.arange(self.dh-local_search_window[2],self.dh+local_search_window[2]+1)
+        max_score = 0.0
+        best_estimate = (0.,0.,0.)
+
+        scan = self.get_next_scan()
+        while True:
+            for gx in grid_x:
+                for gy in grid_y:
+                    for gh in grid_h:
+                        laser_grid = np.zeros_like(self.static_map)
+                        x = gx + scan * np.cos(np.radians(gh) + self.scan_angles)
+                        y = gy + scan * np.sin(np.radians(gh) + self.scan_angles)
+                        x = np.clip(x,0,209)
+                        y = np.clip(y,0,309)
+                        laser_grid[y.astype(np.uint16),x.astype(np.uint16)] = 1.0
+                        score = np.multiply(self.static_map, laser_grid).sum()
+                        if score > max_score:
+                            best_estimate = (gx,gy,gh)
+                            max_score = score
+
+            laser_grid = np.zeros_like(self.static_map)
+            (self.dx,self.dy,self.dh) = best_estimate
+            self.dh = (self.dh+180)%360 - 180
+
+            x = self.dx + scan * np.cos(np.radians(self.dh) + self.scan_angles)
+            y = self.dy + scan * np.sin(np.radians(self.dh) + self.scan_angles)
+            x = np.clip(x,0,209)
+            y = np.clip(y,0,309)
+            laser_grid[y.astype(np.uint16),x.astype(np.uint16)] = 1.0
+            combined = self.static_map + 255*laser_grid
+            cv2.imshow('EST',cv2.resize(combined,None,fx=2,fy=2))
+
+            print(best_estimate, max_score)
+
+
+            key = cv2.waitKey(5)
+            if key == ord('q'):
+                cv2.destroyAllWindows()
+                break
+            else:
+                grid_x = np.arange(self.dx-local_search_window[0],self.dx+local_search_window[0]+1)
+                grid_y = np.arange(self.dy-local_search_window[1],self.dy+local_search_window[1]+1)
+                grid_h = np.arange(self.dh-local_search_window[2],self.dh+local_search_window[2]+1)
+                max_score = 0.0
+                best_estimate = (0.,0.,0.)
+                scan = self.get_next_scan()
+
+                    
+        
+
     def start_manual(self):
         scan = self.get_next_scan()
         print(scan)
-            
+        
+
+        max_score = 0.
         while True:
             #cv2.imshow('Map',self.static_map)
-            laser_grid = np.copy(self.static_map)
-            x = self.dx + scan * np.cos(self.dh + self.scan_angles)
-            y = self.dy + scan * np.sin(self.dh + self.scan_angles)
+            laser_grid = np.zeros_like(self.static_map)
+            x = self.dx + scan * np.cos(np.radians(self.dh) + self.scan_angles)
+            y = self.dy + scan * np.sin(np.radians(self.dh) + self.scan_angles)
             x = np.clip(x,0,209)
             y = np.clip(y,0,309)
-            print(y.max())
-            laser_grid[y.astype(np.uint16),x.astype(np.uint16)] = 0xFF
-            cv2.imshow('MAP',cv2.resize(laser_grid,None,fx=2,fy=2))
+            laser_grid[y.astype(np.uint16),x.astype(np.uint16)] = 1.0
+            score = np.multiply(self.static_map, laser_grid).sum()
+            max_score = max(max_score, score)
+            
+            combined = self.static_map + 255*laser_grid
+            cv2.imshow('MAP',cv2.resize(combined,None,fx=2,fy=2))
 
             key = cv2.waitKey(0)
             if key == ord('q'):
@@ -46,6 +112,7 @@ class LaserLocator():
                 break
             elif key == ord('n'):
                 scan = self.get_next_scan()
+                max_score = 0.
                 print(scan)
             elif key == 82:
                 self.dy += 1
@@ -56,17 +123,18 @@ class LaserLocator():
             elif key == 81:
                 self.dx -= 1
             elif key == ord('r'):
-                self.dh += np.radians(1)
+                self.dh += 1
             elif key == ord('e'):
-                self.dh -= np.radians(1)
-            self.dh = self.dh
-            print(self.dx,self.dy,self.dh)
+                self.dh -= 1
+            self.dh = (self.dh+180)%360 - 180
+            print(f'{self.dx} {self.dy} {self.dh} {score/max_score:0.3f} {score:0.3f}')
 
 
 
 if __name__ == '__main__':
-    locater = LaserLocator()
-    locater.start_manual()
+    locater = LaserLocator(init=(105., 76., -111.))
+    #locater.start_manual()
+    locater.start_grid_based()
 '''    
 
     with open('/home/peter/code/AMSL/amsl_gcs/src/logs/20251119_145433/gcs_145433.log') as logfile:
--- src/pool_map.py
+++ src/pool_map.py
@@ -3,15 +3,19 @@ import cv2
 
 map = np.zeros((310,210),dtype=np.uint8)
 
-pool_points = np.array([[0,0],[0,289],[200,289],[200,218],[144,218],[144,49],[200,49],[200,0]],dtype=np.int32)
+cv2.line(map,(0,0),(0,289),255,1)
+cv2.line(map,(0,289),(200,289),255,1)
+cv2.line(map,(200,218),(144,218),255,1)
+cv2.line(map,(144,218),(144,49),255,1)
+cv2.line(map,(144,49),(200,49),255,1)
+cv2.line(map,(200,0),(0,0),255,1)
 
-pool_points = pool_points + (5,5)
-
-cv2.polylines(map,[pool_points.reshape((-1,1,2))],True,255,1)
+M = np.float32([[1,0,5],[0,1,5]])
+map = cv2.warpAffine(map,M,(map.shape[1],map.shape[0]))
 map = cv2.GaussianBlur(map,(11,11),0)
 map = cv2.normalize(map,None,0,255,cv2.NORM_MINMAX)
 
-cv2.imwrite('survival_pool.png',map)
+cv2.imwrite('/home/peter/code/AMSL/amsl_gcs/src/survival_pool.png',map)
 cv2.imshow('map',map)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
